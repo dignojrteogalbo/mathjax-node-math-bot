dddbf6a8a16a471a0d30867822fd8d2607083065
dignojrteogalbo <teogalbo.dj193@hotmail.com>
2021-01-28 12:48:46 -0700

    Created help command and more robust math substring.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const dotenv = require('dotenv');
 const Discord = require('discord.js');
 const mjAPI = require('mathjax-node-svg2png');
-const toFile = require('data-uri-to-file');
 const fs = require('file-system');
 const md5 = require('md5');
 
@@ -19,15 +18,45 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setPresence({
+    status: 'online',
+    activity: {
+      name: '@ me for help!',
+      type: 'PLAYING',
+      url: 'https://github.com/dignojrteogalbo/mathjax-node-math-bot'
+    }
+  });
 });
 
 client.on('message', msg => {
-  let msgLength = msg.content.length;
+  if (msg.author.bot) return;
+  if (msg.content.includes('```math')) {
+    handleMath(msg);
+  }
+  if (msg.mentions.members.first().id === client.user.id) {
+    msg.author.send(helpMessage);
+  }
+});
+
+const helpMessage = new Discord.MessageEmbed()
+  .setColor('0x809bff')
+  .setTitle('Help')
+  .addField('Documentation', '[MathJax](https://www.npmjs.com/package/mathjax-node) is used to render LaTeX code. For documentation on how to write LaTeX equations, [click here](https://en.wikibooks.org/wiki/LaTeX/Mathematics).')
+  .addFields(
+    { name: 'Syntax', value: 'To render LaTeX code, write syntax in ` ```math {code here}``` ` code blocks with math.' },
+    { name: 'Example', value: '` ```math \\int^b_a f(x)dx``` `'}
+  )
+  .attachFiles('example.png')
+  .setImage('attachment://example.png')
+  .setFooter('Made by @dingo#9485');
+
+const handleMath = (msg) => {
   let channel = msg.channel;
-  if (msg.content.indexOf('```math') !== -1) {
-    if (msg.content.indexOf('```', 7) !== -1) {
-      let content = String.raw`\bbox[white, 10px]{${msg.content.substring(7, msg.content.indexOf('```', 7))}}`;
-    
+  let codeBegin = msg.content.indexOf('```math');
+  if (codeBegin !== -1) {
+    let codeEnd = msg.content.indexOf('```', codeBegin+7);
+    if (codeEnd !== -1) {
+      let content = String.raw`\bbox[white, 10px]{${msg.content.substring(codeBegin+7, codeEnd)}}`;
       if (fs.existsSync(`${md5(content)}.png`)) {
         channel.send({
           files: [{
@@ -43,7 +72,7 @@ client.on('message', msg => {
           png: true,
           scale: 3,
           MathaJax: {
-            tex: {packages: {'[+]': ['bbox']}}
+            tex: { packages: { '[+]': ['bbox'] } }
           }
         }, data => {
           if (!data.errors) {
@@ -59,10 +88,10 @@ client.on('message', msg => {
           } else {
             msg.reply(data.errors);
           }
-        }); 
+        });
       }
     }
   }
-});
+}
 
 client.login(process.env.TOKEN);
